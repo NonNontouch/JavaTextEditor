c3d8565ff7ef95f1ee97e15c121f6b086b7d019b
PodWishu <podwishusriyotee@gmail.com>
2022-04-17 21:47:40 +0700

    hello2

--- src/UI.java
+++ src/UI.java
@@ -3,6 +3,7 @@ import javafx.scene.layout.BorderPane;
 public class UI extends BorderPane {
     public static void main(String[] args) {
         System.out.println("HELLO");
+        System.out.println("HELLO2");
     }
     
 }
