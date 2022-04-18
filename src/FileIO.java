12b91fa4867f7db96c4d1a2f6b7a8f82a323fbc6
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-18 20:46:15 +0700

    Update
    เพิ่มให้สามารถอ่านไฟล์ได้แล้ว แต่ยังติดปัญหาให้ไฟล์ขึ้นบรรทัดใหม่

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,26 +1,62 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
 
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class FileIO {
 
-    private DataInputStream InputFile;
-    private FileChooser chooser;
-    private Stage FileIOStage;
+  private DataInputStream InputStream;
+  private DataOutputStream OutputStream;
 
-    public FileIO(Stage primaryStage) {
-        chooser = new FileChooser();
-        FileIOStage = primaryStage;
+  private TextField TFPane;
+
+  private File Fileopen;
+  private Scanner ReadFile;
+
+  private FileChooser Chooser;
+  private Stage FileIOStage;
+
+  public FileIO(Stage primaryStage, TextField tf) {
+    Chooser = new FileChooser();
+    FileIOStage = primaryStage;
+    TFPane = tf;
+  }
+
+  public TextField OpenFile() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    String temp = new String();
+    Fileopen = Chooser.showOpenDialog(FileIOStage);
+    FileReader Reader = new FileReader(Fileopen);
+    BufferedReader bf = new BufferedReader(Reader);
+
+    if (Fileopen.canRead() && Fileopen.canWrite()) {
+      while (temp != null) {
+        temp = bf.readLine();
+        sb.append(temp);
+        sb.append(System.lineSeparator());
+      }
+      TFPane.setText(sb.toString());
+      // ไฟล์สามารถเปิดอ่านและเขียนได้
+      return TFPane;
+
+    } else if (Fileopen.canRead() && !Fileopen.canWrite()) {
+      // ไฟล์เปิดอ่านได้แต่เขียนไม่ได้
+      return TFPane;
     }
 
-    public void OpenFile(){
-      File fileopen = chooser.showOpenDialog(FileIOStage);
-    }
+    return TFPane;
+  }
 
-    public void SaveFile(){
+  public void SaveFile() {
 
-    }
+  }
 
 }
