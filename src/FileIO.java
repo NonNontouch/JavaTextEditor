b212848b4c1d37d54684c8f76759ca9d4503bf91
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-18 16:28:07 +0700

    Update เพิ่ม Openfile ในFile IO
    เพิ่มในclass fieio ให้สามารถเลือก path ได้

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,6 +1,26 @@
+import java.io.DataInputStream;
+import java.io.File;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class FileIO {
-    
-    public FileIO(){
+
+    private DataInputStream InputFile;
+    private FileChooser chooser;
+    private Stage FileIOStage;
+
+    public FileIO(Stage primaryStage) {
+        chooser = new FileChooser();
+        FileIOStage = primaryStage;
+    }
+
+    public void OpenFile(){
+      File fileopen = chooser.showOpenDialog(FileIOStage);
+    }
+
+    public void SaveFile(){
 
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
-public class Main extends Application{
-    public void start(Stage primaryStage){
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
         UI ui = new UI();
-        Scene scene = new Scene(ui,800,600);
+        FileIO fileio = new FileIO(primaryStage);
+        Scene scene = new Scene(ui, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
