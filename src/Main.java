e2c6e6c79cad095c9ac6d24c0ebba9007e7d7bc4
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-18 20:46:54 +0700

    แก้ไขเล็กน้อย
    เพิ่ม TextField สำหรับทดลองอ่านไฟล์ดู

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,18 +14,25 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         BorderPane borderPane = new BorderPane();
-        GridPane gridPane = new GridPane();
-        gridPane.add(btOpen, 0, 0);
-        gridPane.setAlignment(Pos.TOP_LEFT);
+        TextField panetf = new TextField();
+        panetf.setPrefHeight(500);
+        FileIO fileio = new FileIO(primaryStage,panetf);
+        
 
         UI ui = new UI();
+        ui.setTop(btOpen);
+        ui.setCenter(panetf);
+        
+        btOpen.setAlignment(Pos.TOP_LEFT);
+        btOpen.setOnAction(e -> {
+            try {
+                fileio.OpenFile();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
-        FileIO fileio = new FileIO(primaryStage);
-
-        btOpen.setOnAction(e -> fileio.OpenFile());
-        borderPane.setCenter(gridPane);
-        Scene scene = new Scene(borderPane, 800, 600);
-        //Scene scene = new Scene(ui, 800, 600);
+        Scene scene = new Scene(ui, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
