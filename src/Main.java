1d0f9bb1c5235b85c59503eb50ba48c7ee428a9e
64011106 <64011106@kmitl.ac.th>
2022-04-19 19:13:03 +0700

    Make it work

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private Button btOpen = new Button("Open");
-
     @Override
     public void start(Stage primaryStage) {
-        UI ui = new UI();
+        UI ui = new UI(primaryStage);
         Scene scene = new Scene(ui, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
--- src/UI.java
+++ src/UI.java
@@ -6,55 +6,122 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class UI extends BorderPane {
-    
-    public UI() {
-      MenuBar menuBar = new MenuBar();
+    private static TextArea textArea;
+    private static Stage stage;
+    private static MenuBar menuBar;
 
-      MenuItem iNew = new MenuItem("New");
-      iNew.setOnAction(e -> Controller.onNew());
+    public UI(Stage stage) {
+        UI.stage = stage;
 
-      MenuItem iOpen = new MenuItem("Open");
-      iOpen.setOnAction(e -> Controller.onOpen());
+        initMenuBar();
+        setTop(menuBar);
 
-      MenuItem iSave = new MenuItem("Save");
-      iSave.setOnAction(e -> Controller.onSave());
-
-      MenuItem iSaveAs = new MenuItem("Save As");
-      iSaveAs.setOnAction(e -> Controller.onSaveAs());
-
-      MenuItem iExit = new MenuItem("Exit");
-      iExit.setOnAction(e -> Controller.onExit());
-
-      final Menu fileMenu = new Menu("File", null, iNew, iOpen, iSave, iSaveAs, iExit);
-
-      MenuItem iUndo = new MenuItem("Undo");
-      iUndo.setOnAction(e -> Controller.onUndo());
-
-      MenuItem iRedo = new MenuItem("Redo");
-      iRedo.setOnAction(e -> Controller.onRedo());
-
-      MenuItem iCut = new MenuItem("Cut");
-      iCut.setOnAction(e -> Controller.onCut());
-
-      MenuItem iCopy = new MenuItem("Copy");
-      iCopy.setOnAction(e -> Controller.onCopy());
-
-      MenuItem iPaste = new MenuItem("Paste");
-      iPaste.setOnAction(e -> Controller.onPaste());
-
-      final Menu editMenu = new Menu("Edit", null, iUndo, iRedo, iCut, iCopy, iPaste);
-
-      final Menu viewMenu = new Menu("View", null);
-      final Menu helpMenu = new Menu("Help", null);
-
-      
-      menuBar.getMenus().addAll(fileMenu, editMenu, viewMenu, helpMenu);
-      setTop(menuBar);
-
-      TextArea textArea = new TextArea();
-      setCenter(textArea);
+        textArea = new TextArea();
+        setCenter(textArea);
 
     }
 
-    
+    private void initMenuBar() {
+        menuBar = new MenuBar();
+
+        MenuItem iNew = new MenuItem("New");
+        iNew.setOnAction(e -> onNew());
+
+        MenuItem iOpen = new MenuItem("Open");
+        iOpen.setOnAction(e -> onOpen());
+
+        MenuItem iSave = new MenuItem("Save");
+        iSave.setOnAction(e -> onSave());
+
+        MenuItem iSaveAs = new MenuItem("Save As");
+        iSaveAs.setOnAction(e -> onSaveAs());
+
+        MenuItem iExit = new MenuItem("Exit");
+        iExit.setOnAction(e -> onExit());
+
+        final Menu fileMenu = new Menu("File", null, iNew, iOpen, iSave, iSaveAs, iExit);
+
+        MenuItem iUndo = new MenuItem("Undo");
+        iUndo.setOnAction(e -> onUndo());
+
+        MenuItem iRedo = new MenuItem("Redo");
+        iRedo.setOnAction(e -> onRedo());
+
+        MenuItem iCut = new MenuItem("Cut");
+        iCut.setOnAction(e -> onCut());
+
+        MenuItem iCopy = new MenuItem("Copy");
+        iCopy.setOnAction(e -> onCopy());
+
+        MenuItem iPaste = new MenuItem("Paste");
+        iPaste.setOnAction(e -> onPaste());
+
+        final Menu editMenu = new Menu("Edit", null, iUndo, iRedo, iCut, iCopy, iPaste);
+
+        final Menu viewMenu = new Menu("View", null);
+        final Menu helpMenu = new Menu("Help", null);
+
+        menuBar.getMenus().addAll(fileMenu, editMenu, viewMenu, helpMenu);
+    }
+
+    public static void onNew() {
+        System.out.println("New");
+    }
+
+    public static void onOpen() {
+        FileIO fileIO = new FileIO(stage, textArea);
+        fileIO.OpenFile();
+    }
+
+    public static void onSave() {
+        System.out.println("Save");
+    }
+
+    public static void onSaveAs() {
+        System.out.println("Save As");
+    }
+
+    public static void onExit() {
+        System.exit(0);
+    }
+
+    public static void onUndo() {
+        System.out.println("Undo");
+    }
+
+    public static void onRedo() {
+        System.out.println("Redo");
+    }
+
+    public static void onCut() {
+        System.out.println("Cut");
+    }
+
+    public static void onCopy() {
+        System.out.println("Copy");
+    }
+
+    public static void onPaste() {
+        System.out.println("Paste");
+    }
+
+    public static void onZoomIn() {
+        System.out.println("Zoom In");
+    }
+
+    public static void onZoomOut() {
+        System.out.println("Zoom Out");
+    }
+
+    public static void onZoomReset() {
+        System.out.println("Zoom Reset");
+    }
+
+    public static void onAbout() {
+        System.out.println("About");
+    }
+
+    public static void onHelp() {
+        System.out.println("Help");
+    }
 }
