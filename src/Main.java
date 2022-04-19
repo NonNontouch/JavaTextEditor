45a30ffeee7745be8ebf117f69e942d4ecaa54ce
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2022-04-19 20:19:12 +0700

    Fix warning

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
-    UI ui = new UI(primaryStage);
+    new UI(primaryStage);
   }
 
   public static void main(String[] args) {
