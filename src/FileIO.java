f971ed2d80d7c7c336fb29c20dff79a7c384247a
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-20 09:20:09 +0700

    Update FileIO
    เพิ่มการอ่านแบบ ReadOnly File
    แต่มันยังมีบั๊คที่เปิดไฟล์แปลกๆเช่น .doc
    .pdf ซึ่ง notepad เปิดได้(แต่อ่านไม่รู้เรื่อง)
    แต่ถ้ามาเปิดในโปรแกรมนี้ มันจะค้างเลย

--- src/FileIO.java
+++ src/FileIO.java
@@ -2,15 +2,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class FileIO {
 
-  private TextArea TFPane;
+  private TextArea TextAreaUI;
 
-  private File Fileopen;
+  private File UserInputFile;
 
   private FileChooser Chooser;
   private Stage FileIOStage;
@@ -18,20 +20,20 @@ public class FileIO {
   public FileIO(Stage primaryStage, TextArea tf) {
     Chooser = new FileChooser();
     FileIOStage = primaryStage;
-    TFPane = tf;
+    TextAreaUI = tf;
   }
 
   public void OpenFile() {
 
     StringBuilder texttoshow = new StringBuilder();
     String temp = "";
-    Fileopen = Chooser.showOpenDialog(FileIOStage);
+    UserInputFile = Chooser.showOpenDialog(FileIOStage);
     // ไฟล์สามารถเปิดอ่านและเขียนได้
-    if (Fileopen!=null) {
-      if (Fileopen.canRead() && Fileopen.canWrite()) {
+    if (UserInputFile != null) {
+      if (UserInputFile.canRead() && UserInputFile.canWrite()) {
         try (
 
-            FileReader Reader = new FileReader(Fileopen);
+            FileReader Reader = new FileReader(UserInputFile);
             BufferedReader bf = new BufferedReader(Reader);) {
 
           while (temp != null) {
@@ -39,21 +41,42 @@ public class FileIO {
             texttoshow.append(temp);
             texttoshow.append(System.lineSeparator());
             temp = bf.readLine();
-            
+
           }
 
-          TFPane.setText(texttoshow.toString());
-
-          bf.close();
-          Reader.close();
+          TextAreaUI.setText(texttoshow.toString());
 
         } catch (Exception e) {
           e.printStackTrace();
         }
 
-      } else if (Fileopen.canRead() && !Fileopen.canWrite()) {
+      } else if (UserInputFile.canRead() && !UserInputFile.canWrite()) {
         // ไฟล์เปิดอ่านได้แต่เขียนไม่ได้
 
+        try (
+
+            FileReader Reader = new FileReader(UserInputFile);
+            BufferedReader bf = new BufferedReader(Reader);) {
+
+          Alert alert = new Alert(AlertType.INFORMATION);
+          while (temp != null) {
+
+            texttoshow.append(temp);
+            texttoshow.append(System.lineSeparator());
+            temp = bf.readLine();
+
+          }
+
+          TextAreaUI.setText(texttoshow.toString());
+          TextAreaUI.setEditable(false);
+
+          alert.setHeaderText(null);
+          alert.setContentText("This file is read only");
+          alert.show();
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
     }
 
