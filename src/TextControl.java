32093e29130590cc6974d0e0d00ef297ed5c41b6
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-21 14:45:26 +0700

    Create TextControl.java

---
+++ src/TextControl.java
@@ -0,0 +1,16 @@
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+
+public class TextControl {
+
+    private TextArea textarea;
+    private Scene textcontrolscene;
+
+    public TextControl(Scene inputscene ,TextArea inputTextArea) {
+
+        this.textarea = inputTextArea;
+        this.textcontrolscene = inputscene;
+
+    }
+
+}
