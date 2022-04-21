0ee55f625abcac9313f540b8a1e7021fbc68e3b0
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-21 17:19:05 +0700

    Update TextControl
    -สามารถpopup หน้าต่างใหม่ทับซ้อนขึ้นมาได้แล้ว

--- src/TextControl.java
+++ src/TextControl.java
@@ -1,16 +1,47 @@
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class TextControl {
 
     private TextArea textarea;
-    private Scene textcontrolscene;
+    private static Stage textStage;
 
-    public TextControl(Scene inputscene ,TextArea inputTextArea) {
+    public TextControl(Stage inputstage, TextArea inputTextArea) {
 
         this.textarea = inputTextArea;
-        this.textcontrolscene = inputscene;
+        this.textStage = inputstage;
 
     }
 
+    public static void TextControlEvent() {
+
+        Label label = new Label("Test New Windod for textControler");
+
+        StackPane secondaryLayout = new StackPane();
+        secondaryLayout.getChildren().add(label);
+
+        Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+        // สร้าง stage ใหม่
+        Stage textcontrolstage = new Stage();
+        textcontrolstage.setTitle("Second Stage");
+        textcontrolstage.setScene(secondScene);
+
+        // ตั้งให้ว่าถ้าไม่ปิดwindow นี้ก็จะทำอะไรไม่ได้
+        textcontrolstage.initModality(Modality.WINDOW_MODAL);
+
+        // ตั้งให้เป็น window ลูก
+        textcontrolstage.initOwner(textStage);
+
+        // เซทตำแหน่งให้อยู่ซ้ายบนเสมอ
+        textcontrolstage.setX(textStage.getX());
+        textcontrolstage.setY(textStage.getY());
+
+        textcontrolstage.show();
+
+    }
 }
