32e7b9960e602ef533eb4c4b8ac70944754c1ba9
MMaddoxx <87891744+NonNontouch@users.noreply.github.com>
2022-05-02 09:33:09 +0700

    Add more Test Files

---
+++ TestFiles/CSharp.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld
+{
+    class Csharp {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TestFiles/GO.go
@@ -0,0 +1,6 @@
+package GO
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
